96c8abf175ddaab45ed0ef3de9ae193158ffdbc9
AidenStripes <84007924+AidenStripes@users.noreply.github.com>
2025-06-03 18:31:11 +0200

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -10,9 +10,6 @@ export default function About() {
   return (
     <div className={styles.container}>
     	<h3>about</h3>
-      My name is Bartosz and I'm from Kraków, Poland. <br/>I'm rappin' in my native language which is a polish language <br />so I want to post the translations
-      of my lyrics (in english) <br />and my thoughts.<br/> Remember rap is starting from the thoughts. <br />If you want some co-op you are invited.<br />
-      Send me a message on my instagram. <br />Peace
 		<h3 className={styles.backToHome}>
         	<Link href="/">
         		← Back to home
